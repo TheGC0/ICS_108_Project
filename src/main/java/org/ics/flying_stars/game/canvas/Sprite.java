5e9d865e57acd714f26619ecd3d5fc64a72280e8
omar- <omar-ask@outlook.com>
2024-12-06 17:32:13 +0300

    Implemented Sprite class

--- src/main/java/org/ics/flying_stars/game/canvas/Sprite.java
+++ src/main/java/org/ics/flying_stars/game/canvas/Sprite.java
@@ -5,18 +5,26 @@ import javafx.scene.canvas.GraphicsContext;
 import org.ics.flying_stars.game.collision.Collidable;
 
 public class Sprite implements Drawable, Collidable {
+    private final Collidable collidable;
+    private final Drawable drawable;
+
+    public Sprite(Drawable drawable, Collidable collidable) {
+        this.collidable = collidable;
+        this.drawable = drawable;
+    }
+
     @Override
     public void draw(GraphicsContext context) {
-
+        drawable.draw(context);
     }
 
     @Override
     public boolean detectCollision(Collidable otherCollidable) {
-        return false;
+        return collidable.detectCollision(otherCollidable);
     }
 
     @Override
     public Bounds getBounds() {
-        return null;
+        return collidable.getBounds();
     }
 }

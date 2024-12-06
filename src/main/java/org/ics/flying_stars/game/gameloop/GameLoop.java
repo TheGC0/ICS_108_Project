7f3d70159c22fcbfd65a34b8c3ccc73c1d251696
omar- <omar-ask@outlook.com>
2024-12-06 18:19:08 +0300

    Added a general event filter in the GameLoop

--- src/main/java/org/ics/flying_stars/game/gameloop/GameLoop.java
+++ src/main/java/org/ics/flying_stars/game/gameloop/GameLoop.java
@@ -1,35 +1,48 @@
 package org.ics.flying_stars.game.gameloop;
 
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.scene.canvas.GraphicsContext;
+
 import org.ics.flying_stars.game.collision.Collidable;
 import org.ics.flying_stars.game.canvas.Drawable;
+
+import javafx.animation.Animation;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
 import javafx.animation.KeyFrame;
 import javafx.util.Duration;
-import java.util.ArrayList;
 
-public class GameLoop{
-    private final GraphicsContext graphicsContext;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class GameLoop {
+    private final Canvas gameCanvas;
     private final ArrayList<Collidable> collidables;
     private final ArrayList<Drawable> drawables;
     private final Timeline gameLoop;
+    private final Deque<Event> gameEvents;
 
     private int frames;
 
-    public GameLoop(int framesPerSecond, GraphicsContext canvasGraphicsContext) {
+    public GameLoop(int framesPerSecond, Canvas canvas) {
         // Set frames and graphics context
-        graphicsContext = canvasGraphicsContext;
+        gameCanvas = canvas;
         frames = framesPerSecond;
 
         // Initialize default values
         drawables = new ArrayList<>();
         collidables = new ArrayList<>();
+        gameEvents = new ArrayDeque<>();
 
         // Set up loop
         gameLoop = new Timeline();
         gameLoop.setCycleCount(Timeline.INDEFINITE);
         setupLoop();
+
+        // Set up game event filter
+        setupGameEventsFilter();
     }
 
     private void setupLoop() {
@@ -41,6 +54,27 @@ public class GameLoop{
         // Add to loop
         gameLoop.getKeyFrames().clear();
         gameLoop.getKeyFrames().add(mainFrame);
+
+
+    }
+
+    private void setupGameEventsFilter() {
+        // Catch all events, clone them, and then consume them.
+        // The cloned events will be re-fired the next frame
+        gameCanvas.addEventFilter(
+                Event.ANY,
+                event -> {
+                    // Only catch events if game loop is running
+                    if (gameLoop.getStatus() != Animation.Status.RUNNING) {
+                        return;
+                    }
+                    // Only catch events with a target node
+                    if (event.getTarget() instanceof Node) {
+                        gameEvents.addLast((Event) event.clone());
+                        event.consume();
+                    }
+                }
+        );
     }
 
     public void start() {
@@ -67,15 +101,27 @@ public class GameLoop{
     private void handle(ActionEvent event) {
         drawFrame();
         detectCollisions();
+        fireEvents();
+    }
+
+    private void fireEvents() {
+        // Re-fire all events in gameEvents while emptying queue
+        while (!gameEvents.isEmpty()) {
+            Event gameEvent = gameEvents.removeFirst();
+            
+            // Trigger event
+            Node gameEventTarget = (Node) gameEvent.getTarget();
+            gameEventTarget.fireEvent(gameEvent);
+        }
     }
 
     private void clear() {
         // Get canvas size
-        double width = graphicsContext.getCanvas().getWidth();
-        double height = graphicsContext.getCanvas().getHeight();
+        double width = gameCanvas.getWidth();
+        double height = gameCanvas.getHeight();
 
         // Clear canvas
-        graphicsContext.clearRect(0, 0, width, height);
+        gameCanvas.getGraphicsContext2D().clearRect(0, 0, width, height);
     }
 
     private void drawFrame() {
@@ -84,7 +130,7 @@ public class GameLoop{
 
         // Draw all drawables
         for (Drawable drawable: drawables) {
-            drawable.draw(graphicsContext);
+            drawable.draw(gameCanvas.getGraphicsContext2D());
         }
     }
 

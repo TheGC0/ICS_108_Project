fdbb68880ee14a199ab48c5c0b4d044f987786aa
TheGC0 <Ali_shamah@outlook.com>
2024-12-11 05:17:21 +0300

    class settings

--- src/main/java/org/ics/flying_stars/settings/Settings.java
+++ src/main/java/org/ics/flying_stars/settings/Settings.java
@@ -1,4 +1,23 @@
 package org.ics.flying_stars.settings;
 
 public class Settings {
+    private Difficulty difficulty;
+    private Shape shape;
+
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
 }
